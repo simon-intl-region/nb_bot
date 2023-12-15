3f82b2fc6de2527e57b93f8c71a7b1256bfc0f35
Hiskias <hiskias.melke1@gmail.com>
2023-12-15 12:14:28 +0900

    Add CSV to JSON conversion script and update .gitignore

---
+++ csv_json.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import json
+
+# Read the CSV file
+df = pd.read_csv('./data.csv')
+
+chatroom_links = df['채팅방 링크 (Chatroom link)']
+
+# only keep the # number part of the link with the hashtag
+chatroom_links = chatroom_links.str.split('#').str[1]
+# remove nan
+chatroom_links = chatroom_links.dropna()
+
+# Convert the column to a list
+chatrooms = chatroom_links.tolist()
+
+# append "-4080996432",
+chatrooms.append("-4080996432")
+
+# Save the list as JSON in youth_groups.json
+with open('./youth_groups.json', 'w') as file:
+    file.write(json.dumps(chatrooms))
